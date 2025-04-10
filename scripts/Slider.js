6dd91e0f163f4290fbed43408c1d3d7b3ea63595
Roger-404 <eshubin15@mail.ru>
2025-04-10 15:08:06 +0300

    Create Slider.js

---
+++ scripts/Slider.js
@@ -0,0 +1,33 @@
+class Slider {
+  selectors = {
+    root: '[data-js-slider]',
+    buttonPrev: '[data-js-slider-button-prev]',
+    buttonNext: '[data-js-slider-button-next]'
+  }
+
+  constructor() {
+    this.rootElement = document.querySelector(this.selectors.root)
+    this.buttonPrevElement = document.querySelector(this.selectors.buttonPrev)
+    this.buttonNextElement = document.querySelector(this.selectors.buttonNext)
+    this.bindEvents()
+  }
+
+  onButtonPrevClick = () => {
+    this.rootElement.scrollBy({
+      left: -290
+    })
+  }
+
+  onButtonNextClick = () => {
+    this.rootElement.scrollBy({
+      left: 290
+    })
+  }
+
+  bindEvents() {
+    this.buttonPrevElement.addEventListener('click', this.onButtonPrevClick)
+    this.buttonNextElement.addEventListener('click', this.onButtonNextClick)
+  }
+}
+
+export default Slider
